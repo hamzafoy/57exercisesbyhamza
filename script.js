f9bab464da16ec787a9555ffb9d1872efdcd4f4b
hamzafoy <hamzaoflouisville@gmail.com>
2022-05-10 13:16:04 -0400

    scaffolded react app and installed library for xterm terminal UI for react.

--- script.js
+++
@@ -1,4 +0,0 @@
-import { Terminal } from 'xterm';
-let term = new Terminal();
-term.open(document.getElementById('terminal'));
-term.write('Testing');
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { XTerm } from 'xterm-for-react'
+
+function App() {
+  return (
+    <div className="App">
+     <XTerm />
+    </div>
+  );
+}
+
+export default App;
